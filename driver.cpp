6e548e760a401663c522252ea899004cafa920f9
jlisner <56241539+jlisner@users.noreply.github.com>
2019-10-15 10:20:27 -0400

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include"Saving.h"
+#include "Saving.h"
 #include"Checking.h"
 #include<fstream>
 #include <list>
@@ -13,6 +13,7 @@ void clearFile()  //empties database
 	ofs.open("accounts.txt", std::ofstream::out | std::ofstream::trunc);
 	ofs.close();
 }
+
 void resave(Account ** acc, int c,list<Account*>l)  //resave data to database
 {
 
@@ -26,6 +27,7 @@ void resave(Account ** acc, int c,list<Account*>l)  //resave data to database
 	}
 
 }
+
 int noOfAccounts()  //returns no of lines
 {
 	int count = 0;
@@ -41,6 +43,7 @@ int noOfAccounts()  //returns no of lines
 		return 0;
 
 }
+
 void read(Account ** acc,int c, list <Account*> &l)  //reads data from database
 {
 	string word;
@@ -79,255 +82,250 @@ int main()
 	while (ch != 'N')
 	{
 		system("cls");
-		cout << "\n\n1. Add Client";
-		cout << "\n2. View all Clients";
-		cout << "\n3. View all Checking accounts and their assets";
-		cout << "\n4. View all Saving accounts and their assets";
-		cout << "\n5. Perform a transaction";
-		cout << "\n6. Remove a client";
-		cout << "\n7. Edit a client";
-		cout << "\n'N' to stop";
+		cout <<"\n\t|               Banking Program                 |\n"
+			 <<"\t+-----------------------------------------------+\n"
+			 <<"\t|1. Add Client                                  |\n"
+			 <<"\t|2. View all Clients                            |\n"
+			 <<"\t|3. View all Checking Accounts and Their Assets |\n"
+			 <<"\t|4. View all Saving Accounts and Their Assets   |\n"
+			 <<"\t|5. Perform a Transaction                       |\n"
+			 <<"\t|6. Remove a Client                             |\n"
+			 <<"\t|7. Edit a Client                               |\n"
+			 <<"\t|                                               |\n"
+			 <<"\t|'N' to Stop                                    |\n"
+			 <<"\t+-----------------------------------------------+\n\n";
 
-		cout << "\n\nSelect (1-7|N) : ";
+		cout << "\tSelect (1-7|N):  ";
 		cin >> ch;
 
 		switch(ch)
 		{
-		case '1':
-		{
-			//Saving new clients
-
-			string name,lname,type;
-			cout << "\nEnter First Name of Client : ";
-			cin >> name;
-			cout << "\nEnter Last Name of Client : ";
-			cin >> lname;
-			cout << "\nEnter Type of Account : 'S' for saving/ 'C' for checking ";
-
-			cin >> type;
-
-			//Polymorphism to assign type of accounts
-			Account * a;   //parent class
-			if (type == "S")
+			case '1': //Saving new clients
 			{
-				a = new Saving(10, name,lname,0,count);
+				system("cls");
+				string name,lname,type;
+				cout <<"\n\t|                Adding Client                  |\n"
+					 <<"\t+-----------------------------------------------+\n"
+					 << "\tEnter Full Name of Client: ";
+				cin >> name >> lname;
+				/*cout << "\nEnter Last Name of Client : ";
+				cin >> lname; */
+				cout << "\n\tEnter Type of Account"
+					 <<"\n\t\t'S' for Saving"
+					 <<"\n\t\t'C' for Checking: ";
+				cin >> type;
+	
+				//Polymorphism to assign type of accounts
+				Account * a;   //parent class
 
-				acc.push_back(a);
-				count++;
+				if (type == "S")
+				{
+					a = new Saving(10, name,lname,0,count);
+					acc.push_back(a);
+					count++;
+				}
+				else if(type=="C")
+				{
+					a = new Checking(15, name,lname,0,count);
+					acc.push_back(a);
+					count++;
+				}
+				resave(data, count,acc);
 			}
-			else if(type=="C")
+			case '2': //viewing clients and account
 			{
-		     a = new Checking(15, name,lname,0,count);
-			 acc.push_back(a);
-
-			 count++;
-
-			}
-
-			resave(data, count,acc);
-
-
-
-		}
-		case '2':
-		{
-
-			//viewing clients and account
-			cout << "\n\n------------  ACCOUNTS ------------------";
-			Account* a;
-			list <Account*> ::iterator it;
-			for (it = acc.begin(); it != acc.end(); ++it)
-			{
-				a = *it;
-				a->showDetails();
-			}
-			system("pause");
-			break;
-		}
-		case '3':
-		{
-			//viewing checking accountss
-			cout << "\n\n------------ CHECKING ACCOUNTS ------------------";
-			Account* a;
-			list <Account*> ::iterator it;
-			for (it = acc.begin(); it != acc.end(); ++it)
-			{
-				a = *it;
-				if (a->owner.getAccountType() == "checking")
-                     a->showDetails();
-			}
-
-			system("pause");
-			break;
-		}
-		case '4':
-		{
-			//viewing saving accounts
-
-			Account* a;
-			list <Account*> ::iterator it;
-
-			cout << "\n\n------------ SAVING ACCOUNTS ------------------";
-
-			for (it = acc.begin(); it != acc.end(); ++it)
-			{
-				a = *it;
-				if (a->owner.getAccountType() == "saving")
+				system("cls");
+				cout <<"\n\t|                   Accounts                    |\n"
+					 <<"\t+-----------------------------------------------+\n";
+				Account* a;
+				list <Account*> ::iterator it;
+				for (it = acc.begin(); it != acc.end(); ++it)
+				{
+					a = *it;
 					a->showDetails();
+				}
+				system("pause");
+				break;
 			}
-			system("pause");
-			break;
-		}
-		case '5':
-		{
-
-			//withdraw and depositing cash
-			string name,lname;
-			cout << "Enter the FIRST name of Client? ";
-			cin >> name;
-			cout << "Enter the Last name of Client? ";
-			cin >> lname;
-			bool status = true;
-
-			Account* a;
-			list <Account*> ::iterator it;
-			for (it = acc.begin(); it != acc.end(); ++it)
+			case '3': //viewing checking accountss
 			{
-				a = *it;
-				if (a->owner.getName() == name && a->owner.getLName() == lname)   // searching for client
+				system("cls");
+				cout <<"\n\t|              Checking Accounts                |\n"
+					 <<"\t+-----------------------------------------------+\n";				
+				Account* a;
+				list <Account*> ::iterator it;
+				for (it = acc.begin(); it != acc.end(); ++it)
 				{
-					cout << "D for deposit || W for withdraw ";
-					char t;
-					cin >> t;
-					cout << "Enter Amount";
-					double amount;
-					cin >> amount;
-
-					cout << name;
-					status = false;
-					if (t == 'D' || t=='d')
+					a = *it;
+					if (a->owner.getAccountType() == "checking")
+	                     a->showDetails();
+				}
+	
+				system("pause");
+				break;
+			}
+			case '4': //viewing saving accounts
+			{
+				system("cls");
+				cout <<"\n\t|               Savings Accounts                |\n"
+					 <<"\t+-----------------------------------------------+\n";					
+				Account* a;
+				list <Account*> ::iterator it;
+				for (it = acc.begin(); it != acc.end(); ++it)
+				{
+					a = *it;
+					if (a->owner.getAccountType() == "saving")
+						a->showDetails();
+				}
+				system("pause");
+				break;
+			}
+			case '5': //withdraw and depositing cash
+			{
+				system("cls");
+				string name,lname;
+				cout <<"\n\t|                 Transactions                  |\n"
+					 <<"\t+-----------------------------------------------+\n"
+					 << "\tEnter Full Name of Client: ";
+				cin >> name >> lname;
+				/*cout << "Enter the Last name of Client? ";
+				cin >> lname;*/
+				bool status = true;
+	
+				Account* a;
+				list <Account*> ::iterator it;
+				for (it = acc.begin(); it != acc.end(); ++it)
+				{
+					a = *it;
+					if (a->owner.getName() == name && a->owner.getLName() == lname)   // searching for client
 					{
-						a->deposit(amount);
+						cout << "\n\tEnter D for deposit || W for withdraw: ";
+						char t;
+						cin >> t;
+						cout << "\n\tEnter Amount: ";
+						double amount;
+						cin >> amount;
+	
+						//cout << name;
+						status = false;
+						if (t == 'D' || t=='d')
+						{
+							a->deposit(amount);
+						}
+						else if (t == 'W' || t=='w')
+						{
+							a->withdraw(amount);
+						}
 					}
-					else if (t == 'W' || t=='w')
+				}
+				if (!status)
+				{
+					cout << "\n\t+----------TRANSACTION SUCCESSFULL--------------+\n\n";
+				}
+				if (status)
+				{
+					cout << "\n\t+-------------NO SUCH CLIENT EXISTS-------------+\n\n";
+				}
+				system("pause");
+				resave(data, count, acc);
+				break;
+			}
+			case '6': //deleting a client from database
+			{
+				system("cls");
+				string name,lname;
+				cout <<"\n\t|                   Deletion                    |\n"
+					 <<"\t+-----------------------------------------------+\n"
+					 << "\tEnter Full Name of Client: ";
+				cin >> name >> lname;
+				/*cout << "Enter the Last name of Client? ";
+				cin >> lname;*/
+				bool status = true;
+				Account* a,* del;
+				list <Account*> ::iterator it;
+				for (it = acc.begin(); it != acc.end(); ++it)
+				{
+					a = *it;
+					if (a->owner.getName() == name && a->owner.getLName() == lname)
 					{
-						a->withdraw(amount);
+						del = a;
+						status = false;
 					}
 				}
-
-			}
-			if (!status)
-			{
-				cout << "\n\n------------ TRANSACTION SUCCESSFULL ------------------";
-			}
-			if (status)
-			{
-				cout << "\n\n**********************************No such client exists*********************************";
-			}
-
-			system("pause");
-			resave(data, count, acc);
-			break;
-		}
-		case '6':
-		{
-			//deleting a client from database
-			string name,lname;
-			cout << "Enter the name of Client? ";
-			cin >> name;
-			cout << "Enter the Last name of Client? ";
-			cin >> lname;
-			bool status = true;
-
-			Account* a,* del;
-			list <Account*> ::iterator it;
-			for (it = acc.begin(); it != acc.end(); ++it)
-			{
-				a = *it;
-				if (a->owner.getName() == name && a->owner.getLName() == lname)
+				if (!status)
 				{
-					del = a;
-					status = false;
-
-
+					acc.remove(del); //deleting from linked list
+					cout << "\n\t+-----------DELETION SUCCESSFULL----------------+\n\n";
 				}
-
-			}
-			if (!status)
-			{
-				acc.remove(del);       //deleting from linked list
-
-				cout << "\n\n------------ DELETED ACCOUNT SUCCESSFULLY ------------------\n";
-			}
-
-			if (status)
-			{
-				cout << "\n\n***********************No such client exists***********************************\n";
-			}
-            system("pause");
-			resave(data, count, acc);
-			break;
-		}
-		case '7':
-		{
-			//Editing the Account on basis of names
-
-			string name, lname;
-			cout << "Enter the name of Client you want to edit? ";
-			cin >> name;
-			cout << "Enter the SECOND name of Client you want to edit? ";
-			cin >> lname;
-			bool status = true;
-
-			Account* a, *del;
-			list <Account*> ::iterator it;
-			for (it = acc.begin(); it != acc.end(); ++it)
-			{
-				a = *it;
-				if (a->owner.getName() == name && a->owner.getLName() == lname)
+	
+				if (status)
 				{
-					string fN, lN, aT;
-					int b;
-					cout << "\n\n\nEnter new First Name: ";    //Enter new Data
-					cin >> fN;
-					cout << "\n\n\nEnter new Last Name: ";
-					cin >> lN;
-					cout << "\n\n\nEnter New Balance: ";
-					cin >> b;
-					cout << "\n\n\nEnter new Account Type (saving/checking): ";
-					cin >> aT;
-					a->owner.setName(fN);   // updating the details
-					a->owner.setLName(lN);
-					a->owner.setAccountType(aT);
-					a->balance = b;
-					status = false;
-
+					cout << "\n\t+-------------NO SUCH CLIENT EXISTS-------------+\n\n";
 				}
-
+	            system("pause");
+				resave(data, count, acc);
+				break;
 			}
-
-			if (!status)
+			case '7': //Editing the Account on basis of names
 			{
-				cout << "\n\n------------ EDITED ACCOUNT SUCCESSFULLY ------------------\n";
+				system("cls");
+				string name, lname;
+				cout <<"\n\t|               Editing Account                 |\n"
+					 <<"\t+-----------------------------------------------+\n"
+					 << "\tEnter Full Name of Client: ";
+				cin >> name >> lname;
+				/*cout << "Enter the SECOND name of Client you want to edit? ";
+				cin >> lname;*/
+				bool status = true;
+				Account* a, *del;
+				list <Account*> ::iterator it;
+				for (it = acc.begin(); it != acc.end(); ++it)
+				{
+					a = *it;
+					if (a->owner.getName() == name && a->owner.getLName() == lname)
+					{
+						string fN, lN, aT;
+						int b;
+						cout << "\n\n\nEnter new First Name: ";    //Enter new Data
+						cin >> fN;
+						cout << "\n\n\nEnter new Last Name: ";
+						cin >> lN;
+						cout << "\n\n\nEnter New Balance: ";
+						cin >> b;
+						cout << "\n\n\nEnter new Account Type (Saving/Checking): ";
+						cin >> aT;
+						a->owner.setName(fN);   // updating the details
+						a->owner.setLName(lN);
+						a->owner.setAccountType(aT);
+						a->balance = b;
+						status = false;
+					}
+				}
+	
+				if (!status)
+				{
+					cout << "\n\t+-------------MODIFICATION SUCCESSFULL--------------+\n\n";
+				}
+				if (status)
+				{
+					cout << "\n\t+-------------NO SUCH CLIENT EXISTS-------------+\n\n";
+				}
+				system("pause");
+				resave(data, count, acc);
+				break;
 			}
-			if (status)
+			case 'N':
 			{
-				cout << "\n\n***********************No such client exists***********************************\n";
+				exit(0);  //Exit in case of pressing N
 			}
-			system("pause");
-			resave(data, count, acc);
-			break;
-		}
-		case 'N':
-		{
-			exit(0);  //Exit in case of pressing N
-		}
-		default:
-		{
-			cout << "\n\n Invalid Choice .  ReEnter ";
-			system("pause");
-			resave(data,count,acc);   // resave data
-			break; }
+			default:
+			{
+				cout << "\n\n\tInvalid Choice.  ReEnter\n\n";
+				system("pause");
+				resave(data,count,acc);   // resave data
+				break; 
+			}
 		}
 	}
+	return 0;
 }
